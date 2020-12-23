525c38f751bf975fc75473d2abf91ba80f166143
dsnpack <dsnpack@PAT1955.alterdata.matriz>
2020-12-23 09:56:11 -0300

    adicionando arquivos do back

---
+++ Agendador-Ferias-Back/src/main/java/br/com/alterdata/AgendadorFeriasBackApplication.java
@@ -0,0 +1,20 @@
+/**
+  * @copyright Alterdata Software
+  * @author jonas.dsn.pack
+  * @since 23/12/2020
+  * @description xxxx xxxx x xxx x x x xxx x xxxxx x x xxxx
+*/
+
+package br.com.alterdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorFeriasBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorFeriasBackApplication.class, args);
+	}
+
+}
