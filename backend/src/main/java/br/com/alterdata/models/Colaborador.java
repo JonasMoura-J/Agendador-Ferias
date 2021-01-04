350198c46efdd471639149777ab24c8872701531
JonasMoura-J <jonasmourat9090@gmail.com>
2021-01-04 10:39:33 -0300

    Adicionando assiciação OneToOne entre colaborador e ferias

--- backend/src/main/java/br/com/alterdata/models/Colaborador.java
+++ backend/src/main/java/br/com/alterdata/models/Colaborador.java
@@ -7,6 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -29,6 +32,12 @@ public class Colaborador implements Serializable{
 	
 	private String departamento;
 	
+	@OneToOne
+	@JoinTable (
+				name = "colaborador_ferias",
+				joinColumns = @JoinColumn(name = "ferias_id", referencedColumnName =  "id"),
+				inverseJoinColumns = @JoinColumn(name = "colaborador_id", referencedColumnName = "id")
+				)
 	private Ferias ferias;
 
 	public Colaborador() {
